2d8c1da427db667d3d138db7b03ee5201be0713a
Tchissambou Jérémy <jeremy.tchissambou@datategy.net>
2019-01-28 11:54:35 +0100

    Add function auto update profile picture

--- Geoloc/change-pp.php
+++ Geoloc/change-pp.php
@@ -4,7 +4,7 @@ mb_internal_encoding('UTF-8');
 if(isset($_POST['btn-upload']))
 {    
      
-	$file = rand(0,100)."-Pprofile-".$_FILES['file']['name'];
+	$file = rand(0,100)."-Pimg-".$_FILES['file']['name'];
     $file_loc = $_FILES['file']['tmp_name'];
 	$file_size = $_FILES['file']['size'];
 	$file_type = $_FILES['file']['type'];
@@ -22,36 +22,47 @@ if(isset($_POST['btn-upload']))
 	
 	if(move_uploaded_file($file_loc,$folder.$final_file))
 	{     
-              try{
-$connexion = new PDO('mysql:host=localhost;dbname=geoloc2;charset=utf8', 'root', 'root');
-} catch (PDOException $e) {
-	echo 'Echec lors de la connexion : ' .$e->getMessage();
+        try{
+            $connexion = new PDO('mysql:host=localhost;dbname=geoloc2;charset=utf8', 'root', 'root');
+        } 
+        catch (PDOException $e) {
+	        echo 'Echec lors de la connexion : ' .$e->getMessage();
+        }
+        $req = $connexion->query("SELECT * FROM documents WHERE doc='PP' AND id_user='".$_SESSION['jeunes']['id']."'") ;
+        $count = $req->rowCount();
+ 
+        if ($count == 1) {
+            $req = $connexion->query("UPDATE documents SET file ='".$new_file_name."' WHERE doc='PP' AND id_user='".$_SESSION['jeunes']['id']."'");
+	?>
+		    <script>
+		        alert('successfully uploaded -- 1');
+                window.location.href='./profile.php?success';
+            </script>
+<?php
+        } else if ($count == 0) {
+            $req = $connexion->prepare("INSERT INTO documents(file,type,id_user,doc,date_ajout) VALUES(:file,:type,:id,:doc, NOW())");
+            $req->execute(array(
+                ':file' => $final_file,
+                ':type' => $file_type,
+                ':id' => $_SESSION['jeunes']['id'],
+                ':doc' => PP
+            ));
+    ?>
+        <script>
+		    alert('successfully uploaded -- 2');
+            window.location.href='./profile.php?success';
+        </script>
+<?php
+    }  
 }
 
-// INSERT INTO documents(file,type,id_user,doc,date_ajout) VALUES(:file,:type,:id,:doc, NOW())
-
-		$req = $connexion->query("UPDATE documents SET file ='".$new_file_name."' WHERE doc='PP' AND id_user='".$_SESSION['jeunes']['id']."'");
-		// $req->execute(array(
-        //     ':file' => $final_file,
-        //     ':type' => $file_type,
-        //     ':id' => $_SESSION['jeunes']['id'],
-        //     ':doc' => PP
-        // ));
-		?>
+	else {
+	?>
 		<script>
-		alert('successfully uploaded');
-        window.location.href='./profile.php?success';
+		    alert('error while uploading file');
+            window.location.href='./profile.php?fail';
         </script>
-		<?php
-	}
-	else
-	{
-		?>
-		<script>
-		alert('error while uploading file');
-        window.location.href='./profile.php?fail';
-        </script>
-		<?php
+<?php
 	}
 }
 ?>
